942fef5503532164225ff4452f62f07329578154
gaelcharles <gael.charles@gmail.com>
2019-12-14 15:48:10 +0100

    Sidebar user inputs for trains dashboard

--- ui.R
+++ ui.R
@@ -1,24 +1,58 @@
+getStationNames <- function() {
+  # Retrieve raw data
+  trains = read.csv("data/french-sncf-trains-regularities/full_trains.csv")
+  
+  # Get unique values for "departure_station", sorted
+  stations <- trains %>% 
+    select(departure_station) %>% 
+    distinct() %>% 
+    arrange(departure_station) %>%
+    rename(Station = departure_station)
+  
+  return(stations)
+}
+
 fluidPage(
   
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  #   # tags$head(
+  #   #   tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+  #   # ),
+  #   
+  #   # includeCSS("style.css"),
   
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
+  navbarPage("Trains and flights dashboards",
     
-    # Show a plot of the generated distribution
-    mainPanel(
-      tabsetPanel(type = "tabs",
-                  tabPanel("Plot", plotOutput("distPlot")),
-                  tabPanel("Summary", verbatimTextOutput("summary"))
-      )
+    tabPanel("Trains",
+      sidebarPanel(
+        h3("Trains control panel"),
+        hr(),
+        selectInput("departure_station",
+                    label = h4("Departure station"),
+                    choices = getStationNames()),
+        sliderInput("year_range",
+                    label = h4("Year range"),
+                    min = 2015,
+                    max = 2018,
+                    value = c(2015, 2018),
+                    ticks = FALSE,
+                    sep = ""),
+        div(class="aggregation_type_class",
+            radioButtons("aggregation_type",
+                         label = NULL,
+                         choices=c("Total aggregations"          = "tot",
+                                   "Average/median aggregations" = "avg",
+                                   "Proportion aggregations (%)" = "pct"))
+        )
+      ),
+      mainPanel(h3("Future plots"))
+    ),
+    tabPanel("Flights",
+     sidebarPanel(
+       h3("Flights control panel"),
+       hr()
+     ),
+     mainPanel(h3("Future plots"))
     )
   )
 )
+
